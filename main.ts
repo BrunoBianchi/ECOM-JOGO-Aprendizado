f7118403ac933ed3cce06be05ab355de41b00dad
Fábio Pereira da Silva Stolf <98622274+FabioStolf25@users.noreply.github.com>
2023-06-01 19:48:48 -0300

    Sprite player, colisões, dificuldade a cada score

--- main.ts
+++ main.ts
@@ -1,19 +1,94 @@
-sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+namespace SpriteKind {
+    export const TiroduMAl = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . 1 . . . . . . . . . . . . 1 . 
+        . 9 . . . . . . . . . . . . 9 . 
+        1 9 1 . . . . . . . . . . 1 9 1 
+        9 6 9 . . . . . . . . . . 9 6 9 
+        6 8 6 8 . . . . . . . . 8 6 8 6 
+        8 . 8 . . . . . . . . . . 8 . 8 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, personagem, 0, -75)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.TiroduMAl, function (sprite, otherSprite) {
+    let mySprite: Sprite = null
+    scene.cameraShake(4, 500)
     sprites.destroy(mySprite)
-    info.setLife(info.life() - 1)
+    info.changeLifeBy(-1)
+    if (info.life() == 0) {
+        game.gameOver(false)
+    }
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(myEnemy)
+    sprites.destroy(projectile)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    scene.cameraShake(4, 500)
+    sprites.destroy(myEnemy)
+    info.changeLifeBy(-1)
+    if (info.life() == 0) {
+        game.gameOver(false)
+    }
 })
 let localizacao_aleatoria = 0
-let mySprite: Sprite = null
+let myEnemy: Sprite = null
+let projectile: Sprite = null
+let personagem: Sprite = null
 effects.starField.startScreenEffect()
-let personagem = sprites.create(assets.image`player`, SpriteKind.Player)
+personagem = sprites.create(assets.image`personagem`, SpriteKind.Player)
+animation.runImageAnimation(
+personagem,
+assets.animation`player`,
+100,
+true
+)
 personagem.y = 100
 personagem.setStayInScreen(true)
 info.setLife(5)
+info.setScore(0)
 controller.moveSprite(personagem, 100, 0)
-game.onUpdateInterval(2000, function () {
-    mySprite = sprites.create(assets.image`enemy1`, SpriteKind.Enemy)
+game.onUpdateInterval(1000, function () {
+    if ((0 as any) >= (25 as any)) {
+        localizacao_aleatoria = randint(1, scene.screenWidth())
+        myEnemy.setPosition(localizacao_aleatoria, 7)
+        myEnemy.setVelocity(0, 50)
+        myEnemy.startEffect(effects.fire, 1300)
+    }
+})
+game.onUpdateInterval(800, function () {
+    if ((0 as any) >= (50 as any)) {
+        personagem.y = 120
+        localizacao_aleatoria = randint(1, scene.screenWidth())
+        myEnemy.setPosition(localizacao_aleatoria, 7)
+        myEnemy.setVelocity(0, 60)
+        myEnemy.startEffect(effects.fire, 1300)
+    }
+})
+game.onUpdateInterval(800, function () {
+    if ((0 as any) >= (75 as any)) {
+        localizacao_aleatoria = randint(2, scene.screenWidth())
+        myEnemy.setPosition(localizacao_aleatoria, 4)
+        myEnemy.setVelocity(0, 60)
+        myEnemy.startEffect(effects.fire, 1300)
+    }
+})
+game.onUpdateInterval(1500, function () {
+    myEnemy = sprites.create(assets.image`enemy1`, SpriteKind.Enemy)
     localizacao_aleatoria = randint(1, scene.screenWidth())
-    mySprite.setPosition(localizacao_aleatoria, 7)
-    mySprite.setVelocity(0, 50)
-    mySprite.startEffect(effects.fire)
+    myEnemy.setPosition(localizacao_aleatoria, 7)
+    myEnemy.setVelocity(0, 50)
+    myEnemy.startEffect(effects.fire, 1300)
 })
