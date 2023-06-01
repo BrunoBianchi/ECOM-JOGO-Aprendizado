c8665f0d5021518c3f4c4d95136a5b15e6f8a729
BrunoBianchi <81328873+BrunoBianchi@users.noreply.github.com>
2023-06-01 18:52:28 -0300

    algumas coisas a mais

--- main.ts
+++ main.ts
@@ -1,19 +1,67 @@
+namespace SpriteKind {
+    export const empty = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    bala = sprites.create(assets.image`myImage`, SpriteKind.Projectile)
+    bala.setPosition(personagem.x, personagem.y)
+    bala.setVelocity(0, -50)
+})
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
-    sprites.destroy(mySprite)
     info.setLife(info.life() - 1)
+    sprites.destroy(mySprite)
+})
+sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
+    animation.runImageAnimation(
+    empty_enemy,
+    assets.animation`explosao`,
+    200,
+    false
+    )
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    Vida_Inimiga.value = Vida_Inimiga.value - 10
+    sprites.destroy(bala)
+    if (Vida_Inimiga.value == 0) {
+        sprites.destroy(mySprite)
+    }
 })
 let localizacao_aleatoria = 0
+let Vida_Inimiga: StatusBarSprite = null
+let empty_enemy: Sprite = null
 let mySprite: Sprite = null
+let bala: Sprite = null
+let personagem: Sprite = null
 effects.starField.startScreenEffect()
-let personagem = sprites.create(assets.image`player`, SpriteKind.Player)
+personagem = sprites.create(assets.image`player`, SpriteKind.Player)
 personagem.y = 100
 personagem.setStayInScreen(true)
 info.setLife(5)
 controller.moveSprite(personagem, 100, 0)
 game.onUpdateInterval(2000, function () {
     mySprite = sprites.create(assets.image`enemy1`, SpriteKind.Enemy)
-    localizacao_aleatoria = randint(1, scene.screenWidth())
+    Vida_Inimiga = statusbars.create(20, 4, StatusBarKind.Health)
+    Vida_Inimiga.attachToSprite(mySprite)
+    localizacao_aleatoria = randint(1, scene.screenWidth() - 20)
+    empty_enemy = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.empty)
+    empty_enemy.follow(mySprite)
     mySprite.setPosition(localizacao_aleatoria, 7)
-    mySprite.setVelocity(0, 50)
+    mySprite.setVelocity(0, 44)
     mySprite.startEffect(effects.fire)
 })
